50aedd3197a5668d78863dc7e1da23b935b54027
Bayan Elshaer <bayanhsh7@gmail.com>
2026-06-28 02:01:56 +0300

    chore: remove temporary draw.io file

--- app/database.py
+++ app/database.py
@@ -2,14 +2,9 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlmodel import  Session, SQLModel
-# from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///./data/tickets.db"
 engine = create_engine(DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
-
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base = declarative_base()
 
 def create_db_table():
     SQLModel.metadata.create_all(bind=engine)
